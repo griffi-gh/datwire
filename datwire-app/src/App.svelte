7d564290511a7a7062ac5893b71d24bc3bf01bcb
griffi-gh <prasol258@gmail.com>
2023-04-01 22:45:40 +0200

    Initial commit

---
+++ datwire-app/src/App.svelte
@@ -0,0 +1,9 @@
+<style lang="scss">
+  .app {
+    display: contents;
+  }
+</style>
+
+<div class="app">
+  <slot/>
+</div> 
---
+++ datwire-app/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import App from "../App.svelte";
+</script>
+
+<App>
+  <h1>Application</h1>
+</App>
