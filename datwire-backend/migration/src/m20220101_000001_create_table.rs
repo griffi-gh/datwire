9ae476704f7b8a6d8ed8a064aed1b0ffdba845bd
griffi-gh <prasol258@gmail.com>
2023-04-03 20:52:47 +0200

    fuck sea-orm, ill just use sqlx
    fuck sea-orm, ill just use sqlx

--- datwire-backend/migration/src/m20220101_000001_create_table.rs
+++ datwire-backend/migration/src/m20220101_000001_create_table.rs
@@ -9,23 +9,31 @@ impl MigrationTrait for Migration {
         // Replace the sample below with your own migration scripts
         todo!();
 
-        manager
-            .create_table(
-                Table::create()
-                    .table(Post::Table)
-                    .if_not_exists()
-                    .col(
-                        ColumnDef::new(Post::Id)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
-                    )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
-                    .to_owned(),
-            )
-            .await
+        manager.create_table(
+          Table::create()
+            .table(User::Table)
+            .if_not_exists()
+            .col(ColumnDef::new(User::Id).uuid().default())
+            .to_owned()
+        ).await;
+
+        // manager
+        //     .create_table(
+        //         Table::create()
+        //             .table(Post::Table)
+        //             .if_not_exists()
+        //             .col(
+        //                 ColumnDef::new(Post::Id)
+        //                     .integer()
+        //                     .not_null()
+        //                     .auto_increment()
+        //                     .primary_key(),
+        //             )
+        //             .col(ColumnDef::new(Post::Title).string().not_null())
+        //             .col(ColumnDef::new(Post::Text).string().not_null())
+        //             .to_owned(),
+        //     )
+        //     .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
@@ -40,9 +48,10 @@ impl MigrationTrait for Migration {
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Post {
-    Table,
-    Id,
-    Title,
-    Text,
+enum User {
+  Table,
+  Id,
+  Username,
+  Handle,
+
 }
