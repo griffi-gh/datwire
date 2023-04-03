70647402160c73778b8e227e0b0b8f29adda5a48
griffi-gh <prasol258@gmail.com>
2023-04-04 01:19:52 +0200

    update

--- datwire-backend/src/main.rs
+++ datwire-backend/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, net::{SocketAddr, IpAddr}};
+use axum::{Router, routing::get};
+use anyhow::{Result, Context, anyhow};
+
+pub(crate) struct Config {
+  pub addr: IpAddr,
+  pub port: u16,
+  pub database_url: String,
+}
+impl Config {
+  pub fn socket_addr(&self) -> SocketAddr {
+    SocketAddr::new(self.addr, self.port)
+  }
+}
+pub(crate) struct State {
+  config: Config,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+  dotenvy::dotenv()?;
+  tracing_subscriber::fmt::try_init().map_err(|err| anyhow!(err))?;
+
+  let config = Config {
+    addr: env::var("ADDR").unwrap_or("0.0.0.0".into()).parse().with_context(|| "ADDR is invalid")?,
+    port: env::var("PORT").unwrap_or("8080".into()).parse().with_context(|| "PORT is invalid")?,
+    database_url: env::var("DATABASE_URL").with_context(|| "DATABASE_URL not specified")?,
+  };
+
+  // build application
+  let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+  // run it with hyper
+  tracing::info!("server go brrrrr on {}", config.socket_addr());
+  axum::Server::bind(&config.socket_addr())
+    .serve(app.into_make_service())
+    .await?;
+
+  Ok(())
 }
