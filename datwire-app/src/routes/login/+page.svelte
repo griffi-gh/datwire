2534ba209139e6867c4a2669c28dd80b58560502
griffi-gh <prasol258@gmail.com>
2023-04-07 00:49:13 +0200

    add i18n

--- datwire-app/src/routes/login/+page.svelte
+++ datwire-app/src/routes/login/+page.svelte
@@ -12,8 +12,8 @@
 <DialogPage>
   <h1 class="text-2xl text-center m-4">{$_('login_flow.welcome')}</h1>
   <form class="flex flex-col w-[20rem]">
-    <TextInput type="email" name="email" placeholder="user@mail.com" label={$_('common.email')} autocomplete="email" required />
-    <TextInput type="password" name="password" placeholder="Tr0ub4dor&3" label={$_('common.password')} minLength={8} autocomplete="current-password" required/>
+    <TextInput type="email" name="email" placeholder="user@mail.com" label={$_('email')} autocomplete="email" required />
+    <TextInput type="password" name="password" placeholder="Tr0ub4dor&3" label={$_('password')} minLength={8} autocomplete="current-password" required/>
     <div class="my-2 flex flex-col">
       <ActionButton type="submit">{$_('login_flow.btn_login')}</ActionButton>
     </div>
--- datwire-app/src/routes/register/+page.svelte
+++ datwire-app/src/routes/register/+page.svelte
@@ -4,7 +4,8 @@
   import TextInput from "$components/TextInput.svelte";
   import ActionButton from "$components/ActionButton.svelte";
   import ExclamationCircle from 'svelte-heros-v2/ExclamationCircle.svelte';
-    import { goto } from '$app/navigation';
+  import { goto } from '$app/navigation';
+  import { _ } from 'svelte-i18n';
 
   let loading = false;
   let password = "";
@@ -51,13 +52,13 @@
 </svelte:head>
 
 <DialogPage>
-  <h1 class="text-2xl text-center m-4">Join Datwire</h1>
+  <h1 class="text-2xl text-center m-4">{$_('register_flow.welcome')}</h1>
   <form method="post" action={`${PUBLIC_API_HOST}/api/register`} class="flex flex-col w-[50vw] min-w-[20rem] max-w-[40rem]" on:submit|preventDefault={onSubmit}>
-    <TextInput name="handle" placeholder="ViperGamingX" label="Handle" required />
-    <TextInput type="email" name="email" placeholder="user@mail.com" label="Email" required />
+    <TextInput name="handle" placeholder="ViperGamingX" label={$_('handle')} required />
+    <TextInput type="email" name="email" placeholder="user@mail.com" label={$_('email')} required />
     <div class="flex flex-wrap [&>*]:flex-1">
-      <TextInput type="password" name="password" placeholder="Tr0ub4dor&3" label="Password" minLength={8} autocomplete="new-password" required bind:value={password} />
-      <TextInput type="password" name="repeat_password" placeholder="Tr0ub4dor&3" label="Repeat password" minLength={8} pattern={escapeRegex(password)} autocomplete="new-password" required />
+      <TextInput type="password" name="password" placeholder="Tr0ub4dor&3" label={$_('password')} minLength={8} autocomplete="new-password" required bind:value={password} />
+      <TextInput type="password" name="repeat_password" placeholder="Tr0ub4dor&3" label={$_('password_repeat')} minLength={8} pattern={escapeRegex(password)} autocomplete="new-password" required />
     </div>
     {#if error != null}
       <p class="text-pink-700 font-bold px-3 align-middle flex items-center">
@@ -65,10 +66,12 @@
       </p>
     {/if}
     <div class="my-2 flex flex-col">
-      <ActionButton type="submit" loading={loading} disabled={loading}>Create new account</ActionButton>
+      <ActionButton type="submit" loading={loading} disabled={loading}>
+        {$_('register_flow.btn_create')}
+      </ActionButton>
     </div>
   </form>
   <a href="/login" class="text-blue text-center underline block">
-    Log in instead?
+    {$_('register_flow.link_login')}
   </a>
 </DialogPage>
