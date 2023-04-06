0327f1a2b17cdbdf84c9cbbe35e71f96ff33954b
griffi-gh <prasol258@gmail.com>
2023-04-06 05:12:36 +0200

    re-enable email auto-complete

--- datwire-app/src/routes/login/+page.svelte
+++ datwire-app/src/routes/login/+page.svelte
@@ -6,7 +6,7 @@
 <DialogPage>
   <h1 class="text-2xl text-center m-4">Welcome back</h1>
   <form class="flex flex-col">
-    <InputBox type="email" name="email" placeholder="user@mail.com" label="Email" required />
+    <InputBox type="email" name="email" placeholder="user@mail.com" label="Email" autocomplete="email" required />
     <InputBox type="password" name="password" placeholder="Tr0ub4dor&3" label="Password" pattern=".........*" autocomplete="current-password" required />
     <button type="submit">Login</button>
   </form>
--- datwire-app/src/routes/register/+page.svelte
+++ datwire-app/src/routes/register/+page.svelte
@@ -6,8 +6,8 @@
 
 <DialogPage>
   <h1 class="text-2xl text-center m-4">Join Datwire</h1>
-  <form class="flex flex-col" autocomplete="off">
-    <InputBox type="email" name="email" placeholder="user@mail.com" label="Email" required autocomplete="off" />
+  <form class="flex flex-col">
+    <InputBox type="email" name="email" placeholder="user@mail.com" label="Email" required />
     <InputBox type="password" name="password" placeholder="Tr0ub4dor&3" label="Password" pattern=".........*" autocomplete="new-password" required bind:value={ password } />
     <InputBox type="password" name="repeat_password" placeholder="Tr0ub4dor&3" label="Repeat password" autocomplete="new-password" required pattern={ (password.length >= 8) ? password.replace((/[.*+?^${}()|[\]\\]/g), '\\$&') : "(?!)" } />
     <button type="submit">Login</button>
